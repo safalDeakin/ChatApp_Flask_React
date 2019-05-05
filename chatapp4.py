c476eac83b074a6d7c6959036e821b6b946a1e67
Safal Shrestha <safalshrestha@Safals-MacBook-Pro.local>
2019-05-05 14:07:50 +1000

    bug fixes

--- chatapp4.py
+++ chatapp4.py
@@ -14,6 +14,7 @@ def index():
 
 @socketio.on( 'event msg' )
 def incomingMessage(json):
+    print json
     if(json['event']=='EVENT_PRI_MSG'):
         sendPrivateMessage(json['data'])
     elif(json['event']=='EVENT_PUB_MSG'):
@@ -21,26 +22,27 @@ def incomingMessage(json):
     elif(json['event']=='REG_MSG'):
         registerUser(json['data'])
 
-def sendPrivateMessage( json ):
-    print( 'received something : ' + str( json ) )
-    session_id = users[json['user']]
-    msg =   msgAsEvent('EVENT_PRI_MSG_RESPONSE',json['msg']); 
-    socketio.emit( 'private room' , msg, room= session_id )
+def sendPrivateMessage( jsonMessage ):
+    print( 'received private message : ' + str( jsonMessage ) )
+    session_id = users[jsonMessage['receiver']]
+    print(session_id)
+    msg =   msgAsEvent('EVENT_PRI_MSG_RESPONSE',jsonMessage['msg'],jsonMessage['sender']); 
+    socketio.emit( 'server response' , msg, room= session_id )
     
-def registerUser(user_name):
-    print( 'received private msg :' + user_name)
-    users[user_name]= request.sid
+def registerUser(data):
+    print( 'received register msg :' + str(data))
+    users[data['userName']]= request.sid
     print (users)
 
-def msgAsEvent(event,jsonMessage):
-    newMessage = {'event': event, 'msg': jsonMessage}
+def msgAsEvent(event,jsonMessage,sender):
+    newMessage = {'event': event, 'data':{ 'msg': jsonMessage,'sender':sender}}
     print(newMessage)
-    return jsonMessage
+    return newMessage
 
 def sendBroadCastMessage( json ):
     print( 'received something : ' + str( json ) )
     msg =   msgAsEvent('EVENT_PUB_MSG_RESPONSE',json['msg']); 
-    socketio.emit( 'private room' , msg)
+    socketio.emit( 'server response' , msg)
 
 if __name__ == '__main__':
     socketio.run(app,debug =True)
